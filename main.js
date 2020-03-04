4f4d1d162a5c0120972096130fbc1d98716f4e42
Md Golam Jakaria <58362149+MdGolamJakaria@users.noreply.github.com>
2020-03-04 23:27:11 +0600

    space removed

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 
 const submitButton = document.getElementById("submit-button");
     submitButton.addEventListener("click",function(){
-        const password = document.getElementById ("password").Value;
+        const password = document.getElementById("password").Value;
         const passwordNumber = parseFloat(password);
 
 
         document.getElementById("show-password").innerText = passwordNumber;
    
-        document.getElementById ("password").value="";
+        document.getElementById("password").value="";
     })
 
 const resetButton = document.getElementById("reset-button");
