e6bc80c8c46d82783b94a596b9afbdbae3a9e057
Md Golam Jakaria <58362149+MdGolamJakaria@users.noreply.github.com>
2020-03-04 23:18:02 +0600

    problem

---
+++ main.js
@@ -0,0 +1,16 @@
+
+const submitButton = document.getElementById("submit-button");
+    submitButton.addEventListener("click",function(){
+        const password = document.getElementById ("password").Value;
+        const passwordNumber = parseFloat(password);
+
+
+        document.getElementById("show-password").innerText = passwordNumber;
+   
+        document.getElementById ("password").value="";
+    })
+
+const resetButton = document.getElementById("reset-button");
+    resetButton.addEventListener("click", function(){
+        document.getElementById("reset-button").reset();
+    })
